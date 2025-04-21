438d4cfd9255c50d400e15b0e5741b9a5546b8ff
Jair Jales <infowide022@gmail.com>
2025-04-20 22:47:45 -0300

    relatorio_faltas.py

--- relatorio_faltas.py
+++ relatorio_faltas.py
@@ -163,8 +163,8 @@ graf_marcas.update_traces(marker_line_width=1, textposition="outside")
 
 st.plotly_chart(graf_marcas, use_container_width=True)
 
-    st.markdown("### 📋 Tabela Geral de Dados")
-    st.dataframe(df_filtrado[["SKU", "Titulo", "Estoque", "Marca", "Conta_Exibicao", "Faltas"]], use_container_width=True, height=400)
+st.markdown("### 📋 Tabela Geral de Dados")
+st.dataframe(df_filtrado[["SKU", "Titulo", "Estoque", "Marca", "Conta_Exibicao", "Faltas"]], use_container_width=True, height=400)
 
 with tabs[1]:
     st.markdown("## 📈 Evolução das Faltas")
